a1ffad17616c3e46460460c80880b4c120fe54a0
khauta seabata <75206578+KhautaSeabata@users.noreply.github.com>
2025-05-16 06:30:56 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
 import websocket
 import json
-import time
 import requests
 from datetime import datetime
 
-# Firebase config
-FIREBASE_URL = "https://data-364f1-default-rtdb.firebaseio.com/ticks.json"  # <- Replace with your Firebase URL
+# Firebase config (push under V0latility 10)
+FIREBASE_URL = "https://data-364f1-default-rtdb.firebaseio.com/V0latility%2010.json"
 
-# WebSocket handlers
 def on_open(ws):
     print("✅ WebSocket connected.")
     subscribe_message = {
-        "ticks": "R_10",  # Replace with your desired symbol
+        "ticks": "R_10",  # Your desired symbol
         "subscribe": 1
     }
     ws.send(json.dumps(subscribe_message))
@@ -47,8 +45,8 @@ def on_close(ws, close_status_code, close_msg):
 if __name__ == "__main__":
     ws_url = "wss://ws.binaryws.com/websockets/v3?app_id=1089"
     ws = websocket.WebSocketApp(ws_url,
-                                 on_open=on_open,
-                                 on_message=on_message,
-                                 on_error=on_error,
-                                 on_close=on_close)
+                                on_open=on_open,
+                                on_message=on_message,
+                                on_error=on_error,
+                                on_close=on_close)
     ws.run_forever()
