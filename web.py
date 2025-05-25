35ee4428652e99921dc9171fb53626878022e41d
khauta seabata <75206578+KhautaSeabata@users.noreply.github.com>
2025-05-25 14:12:11 -0800

    Update web.py

--- web.py
+++ web.py
@@ -65,7 +65,7 @@ HTML = """
 
 @app.route("/")
 def home():
-    return render_template_string(HTML, symbol="R_150")
+    return render_template_string(HTML, symbol="R_25")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
