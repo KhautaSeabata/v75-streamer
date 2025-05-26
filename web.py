b04149c20afe89902fae37c4876c50ccaea31552
khauta seabata <75206578+KhautaSeabata@users.noreply.github.com>
2025-05-26 03:59:39 -0800

    Update web.py

--- web.py
+++ web.py
@@ -1,5 +1,6 @@
 # web.py
 from flask import Flask, render_template_string, send_file
+import os
 
 app = Flask(__name__)
 
@@ -95,4 +96,6 @@ def candles():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
